f838aff307e6d81f6bff6cd6c2cb426675d280ed
Kayvon Kazemini <kayvonk@gmail.com>
2023-08-17 22:51:46 -0600

    deploy

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('../client/dist'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+
+
+// 1. work on our webpack config
+
+// 2. work on our service worker
+
+// 3. set up indexedDB
+
+// 4. make our app installable
